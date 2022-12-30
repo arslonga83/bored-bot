90370480f5dd2091156707dccbf4213dadb7cb55
arslonga83 <jlwood83@gmail.com>
2022-12-29 21:43:36 -0800

    add delay and sound effect plus media queries for smaller screens

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 
 const activity = document.querySelector('#activity')
+const boredBtn = document.querySelector('#bored-btn')
+
+const sound = new Audio('./little-robot-sound-84657.mp3')
+
+let isWaiting = false;
 
 const getActivity = () => {
-  fetch('https://apis.scrimba.com/bored/api/activity')
-    .then((response) => response.json())
-    .then((data) => activity.innerHTML = data.activity)
+  if (!isWaiting) {
+    isWaiting = true;
+    sound.play();
+    activity.textContent = 'Processing...'
+    setTimeout(() => {
+    fetch('https://apis.scrimba.com/bored/api/activity')
+      .then((response) => response.json())
+      .then((data) => activity.innerHTML = `
+        <span class="animate__animated animate__fadeIn animate__slower">${data.activity}</span>`)
+    isWaiting = false;
+    }, 9000)
+    
+  }
+
 }
 
-
-document.querySelector('#bored-btn').addEventListener('click', getActivity)
+boredBtn.addEventListener('click', getActivity)
